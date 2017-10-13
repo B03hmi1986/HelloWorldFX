8a68b90f0307a684434885db4baedfd82e679b80
Marco Böhmann <marco.boehmann@gmail.com>
2017-10-13 15:08:45 +0200

    New Project HelloWorldFX

---
+++ src/sample/Main.java
@@ -0,0 +1,46 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+
+        //Lyout Container erstellen
+        VBox vbox1 = new VBox();
+        vbox1.setPadding(new Insets(25,25,25,25));
+        vbox1.setSpacing(25);
+
+        //Komponenten erstellen
+        Label label = new Label();
+        Button button1 = new Button("ACTION");
+
+        //Label und Button in den Scene Graph einfügen
+        vbox1.getChildren().add(button1);
+        vbox1.getChildren().add(label);
+
+        //Erstellung der Scene und Hinzufügen zur Stage
+        Scene scene = new Scene(vbox1);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
